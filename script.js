fe0bc023614f733a2e8cd63daeebb37bbac2c542
Mohammed Zaheer <mohammedzaheer6138@gmail.com>
2024-01-24 19:26:55 +0530

    changed

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('#main'),
-//     smooth: true
-// });
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
 
 let Elem1 = document.querySelector("#elem1");
 let ElemC = document.querySelector("#elemcontainer");
